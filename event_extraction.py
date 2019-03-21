e96fa8acab4dbef3a6e80ba98c01dd6052c306a6
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2019-03-21 16:20:51 +0900

    Added build dataset

--- event_extraction.py
+++ event_extraction.py
@@ -1,7 +1,8 @@
-import os.path as osp
-from collections import Counter
+from pathlib import Path
 import pickle
+from collections import Counter
 
+import fire
 import spacy
 from textacy import extract
 from lineflow.core import CsvDataset
@@ -12,6 +13,7 @@ UNK_TOKEN = '<unk>'
 START_TOKEN = '<s>'
 END_TOKEN = '</s>'
 SEP_TOKEN = '<sep>'
+NONE_TOKEN = '<none>'
 
 
 NLP = spacy.load('en_core_web_sm')
@@ -25,9 +27,9 @@ def extract_events(doc, lemmatize=True):
     events = []
     for event in extract.subject_verb_object_triples(doc):
         if lemmatize:
-            event = [item.lemma_ if item else None for item in event]
+            event = [item.lemma_ if item else NONE_TOKEN for item in event]
         else:
-            event = [item.text if item else None for item in event]
+            event = [item.text if item else NONE_TOKEN for item in event]
         events.append(event)
     return events
 
@@ -41,43 +43,73 @@ def preprocess(x):
             x['sentence5'].lower()
             )
     story = [extract_events(NLP(context)) for context in contexts]
-    return {'story': story, 'text': x}
+
+    events = []
+    # Merge events
+    for line in story:
+        event = []
+        event += [START_TOKEN]
+        for tokens in line:
+            event += tokens
+            event += [SEP_TOKEN]
+        event = event[:-1]
+        event += [END_TOKEN]
+        events.append(event)
+
+    return events
 
 
-def build_vocab(tokens, cache='vocab.pkl', max_size=500000):
-    if not osp.isfile(cache):
-        counter = Counter(tokens)
-        words, _ = zip(*counter.most_common(max_size))
-        words = [PAD_TOKEN, UNK_TOKEN, SEP_TOKEN] + list(words)
-        t2i = dict(zip(words, range(len(words))))
-        if START_TOKEN not in t2i:
-            t2i[START_TOKEN] = len(t2i)
-            words += [START_TOKEN]
-        if END_TOKEN not in t2i:
-            t2i[END_TOKEN] = len(t2i)
-            words += [END_TOKEN]
-        with open(cache, 'wb') as f:
-            pickle.dump((t2i, words), f)
-    else:
-        with open(cache, 'rb') as f:
-            t2i, words = pickle.load(f)
+def postprocess(t2i):
+    def _f(events):
+        return [[t2i.get(e, UNK_TOKEN) for e in event] for event in events]
+    return _f
+
+
+def build_vocab(tokens, max_size=500000):
+    counter = Counter(tokens)
+    words, _ = zip(*counter.most_common(max_size))
+    words = [PAD_TOKEN, UNK_TOKEN, SEP_TOKEN] + list(words)
+    t2i = dict(zip(words, range(len(words))))
+    if START_TOKEN not in t2i:
+        t2i[START_TOKEN] = len(t2i)
+        words += [START_TOKEN]
+    if END_TOKEN not in t2i:
+        t2i[END_TOKEN] = len(t2i)
+        words += [END_TOKEN]
+    if PAD_TOKEN not in t2i:
+        t2i[PAD_TOKEN] = len(t2i)
+        words += [PAD_TOKEN]
+    if SEP_TOKEN not in t2i:
+        t2i[SEP_TOKEN] = len(t2i)
+        words += [SEP_TOKEN]
+    if NONE_TOKEN not in t2i:
+        t2i[NONE_TOKEN] = len(t2i)
+        words += [NONE_TOKEN]
     return t2i, words
 
 
-def getdata():
-    path = '/home/takeshita/mnt/DATA/NLP/ROC/ROCStories_winter2.csv'
-    data = CsvDataset(
-            path,
+def build(dpath, savedir):
+    savedir = Path(savedir)
+
+    # path = '/home/takeshita/mnt/DATA/NLP/ROC/ROCStories_winter2.csv'
+    dataset = CsvDataset(
+            dpath,
             header=True
             ).map(preprocess)
 
     tokens = [
             item
-            for story in data
-            for events in story['story']
-            for event in events
+            for story in dataset
+            for event in story
             for item in event
             ]
 
     t2i, words = build_vocab(tokens)
-    return data, t2i, words
+    dataset = dataset.map(postprocess(t2i))
+    dataset.save(savedir / 'dataset.token.pkl')
+    with open(savedir / 'vocab.pkl', 'wb') as f:
+        pickle.dump((t2i, words), f)
+
+
+if __name__ == '__main__':
+    fire.Fire()
